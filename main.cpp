6015ad7d0da982ef64dd17ad8b0160811d22d704
karl07 <1474402804@qq.com>
2019-01-13 02:05:22 +0800

    first commit try

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "complex/1.h"
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	//int aa=123;
+	complex a(13,-14);
+	cout << a << a.length();
+	cout << "hello world" << endl;
+	return 0;
+}
